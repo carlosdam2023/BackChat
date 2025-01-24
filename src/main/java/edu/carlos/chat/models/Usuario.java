47e0ea9d15c4c8226c60c4594f3a35a2cc9ecf3d
carlosdam <charlygm2004@gmail.com>
2025-01-24 17:33:46 +0100

    Commit antes de añadir JWT

---
+++ src/main/java/edu/carlos/chat/models/Usuario.java
@@ -0,0 +1,48 @@
+package edu.carlos.chat.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "usuarios")
+public class Usuario {
+
+    @Id
+    private String id; // Identificador único del usuario
+
+    private String nombre;
+    private LocalDateTime fechaRegistro;
+
+    public Usuario(){}
+
+    public Usuario(String id, String nombre, LocalDateTime fechaRegistro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+}
